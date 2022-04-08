5f701336f45a6059fdfa99ab47263dd1c9b535a1
muhsufyan <muh.sufyan.ts@gmail.com>
2022-04-08 21:24:39 +0700

    buat middleware sederhana untuk request validator

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # typescript-restapi
 https://www.youtube.com/watch?v=BWUi6BS9T5Y<br>
-<h1>buat global route & buat route sederhana</h1>
+<h1>buat middleware sederhana</h1>
 <ul>
-    <li>buat src/routes.ts untuk global route lalu konsume oleh app.ts</li>
+    <li>buat src/middleware/validateResource.ts, request yg datang (input user) akan masuk ke schema (filter request) lalu masuk ke file ini untuk divalidasi dulu.<br>shema sebagai input dari middleware validateResource <br> misal hrs ada email, minimal nama 4 karakter, dll</li>
     <li>buat route /halo di src/routes.ts</li>
+    <li>schema dibuat dg package zod</li>
 </ul>
---
+++ src/middleware/validateResource.ts
@@ -0,0 +1,20 @@
+import { Response, Request, NextFunction } from "express"
+// for schema
+import { AnyZodObject } from "zod"
+// schema as param and validate will return another func
+const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) =>{
+    try {
+        schema.parse({
+            // handle body (data diinput user)
+            body: req.body,
+            // handle query misal localhost?tag="satu" for catch tag="satu"
+            query: req.query,
+            // handle url param misal localhost/:id for catch id
+            param: req.params
+        })
+    } catch (error: any) {
+        return res.status(400).send(error.errors)
+    }
+} 
+
+export default validate
