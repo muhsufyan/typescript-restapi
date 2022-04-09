483966dd22b438907b7f3041f9f1fdc3d4108069
muhsufyan <muh.sufyan.ts@gmail.com>
2022-04-09 08:33:44 +0700

    buat model yg akan di migrasi ke db, hash & compare password dg bcrypt

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # typescript-restapi
 https://www.youtube.com/watch?v=BWUi6BS9T5Y<br>
-<h1>buat middleware sederhana</h1>
+<h1>buat model yg akan di migrasi ke db, hash & compare password dg bcrypt</h1>
 <ul>
-    <li>buat src/middleware/validateResource.ts, request yg datang (input user) akan masuk ke schema (filter request) lalu masuk ke file ini untuk divalidasi dulu.<br>shema sebagai input dari middleware validateResource <br> misal hrs ada email, minimal nama 4 karakter, dll</li>
+    <li>pertama buat src/models/user.model.ts, untuk membuat model user<br>shema sebagai input dari middleware validateResource <br> misal hrs ada email, minimal nama 4 karakter, dll</li>
     <li>buat route /halo di src/routes.ts</li>
     <li>schema dibuat dg package zod</li>
+    <li>src/models/user.model.ts juga untuk hash dan compare password user</li>
 </ul>
---
+++ src/models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import config from 'config';
+
+// kurang mengerti fungsi ini buat apa 30:20
+export interface UserDocument extends mongoose.Document{
+    email: string,
+    name: string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+    comparePassword: Promise<Boolean>
+}
+// schema
+const userSchema = new mongoose.Schema({
+    // schema yg dibuat (request filter)
+    email: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+},{
+    // untuk create_at dan update_at
+    timestamps: true
+}
+)
+// kurang mengerti 30:50
+userSchema.pre("save", async function(next){
+    // tangkap data user di db mungkin
+    let user = this as UserDocument
+    // mungkin untuk password jika tdk ada edit password
+    if(!user.isModified('password')){
+        return next()
+    }
+    // jika password diubah. buat salt untuk bcyrpt
+    const salt = await bcrypt.genSalt(config.get<number>('saltWorkFactor'))
+    // generate password
+    const hashed = await bcrypt.hashSync(user.password, salt)
+    // ganti password user dg password yg tlh di hash
+    user.password = hashed
+    return next()
+})
+// compare password untuk login
+userSchema.methods.comparePassword = async function (candidatePassword:string): Promise<boolean> {
+    // tangkap data user di db mungkin
+    const user = this as UserDocument
+    return bcrypt.compare(candidatePassword, user.password).catch((error)=>false);
+}
+// model (buat tabel/document)
+const UserModel = mongoose.model("User",userSchema)
+
+export default UserModel
